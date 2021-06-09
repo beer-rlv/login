cebdd9aced235ec519a8a29431f7b07173f5affc
beer_rlv <beer_rlv@outlook.com>
2021-06-10 00:14:39 +0900

    create login/logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRiquest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -13,7 +14,7 @@ class AuthController extends Controller
      */
     public function showLogin()
     {
-        return view('login/login_form');
+        return view('login.login_form');
     }
 
     /**
@@ -22,6 +23,33 @@ class AuthController extends Controller
      */
     public function login(LoginFormRiquest $request)
     {
-        
+        $credentials = $request->only('email','password');
+
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->route('home')->with('login_success','ログイン成功しました★');
+        }
+
+        return back()->withErrors([
+            'login_error' => 'メールアドレスかパスワードが間違っています。',
+        ]);
+    }
+
+    /**
+     * ユーザーをアプリケーションからログアウトさせる
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login.show')->with('logout','ログアウトしました！');
     }
 }
